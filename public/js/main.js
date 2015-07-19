b6262f8a1020388c1e91fe267cfd62e390278b17
TakesxiSximada <takesxi.sximada@gmail.com>
2015-07-19 13:28:04 +0900

    mouse hoverを停止

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         return false;
     });
 
-    
+
 
     //Contact Page Map Centering
     var hw = $('header').width() + 50;
@@ -28,53 +28,48 @@ $(document).ready(function(){
         });
     }
 
-   
+
 
 
 
     //Tooltip
-    $("a").mouseover(function(){
+    // $("a").mouseover(function(){
 
-        var attr_title = $(this).attr("data-title");
+    //     var attr_title = $(this).attr("data-title");
 
-        if( attr_title == undefined || attr_title == "") return false;
-        
-        $(this).after('<span class="tooltip"></span>');
+    //     if( attr_title == undefined || attr_title == "") return false;
 
-        var tooltip = $(".tooltip");
-        tooltip.append($(this).data('title'));
+    //     $(this).after('<span class="tooltip"></span>');
 
-         
-        var tipwidth = tooltip.outerWidth();
-        var a_width = $(this).width();
-        var a_hegiht = $(this).height() + 3 + 4;
+    //     var tooltip = $(".tooltip");
+    //     tooltip.append($(this).data('title'));
 
-        //if the tooltip width is smaller than the a/link/parent width
-        if(tipwidth < a_width){
-            tipwidth = a_width;
-            $('.tooltip').outerWidth(tipwidth);
-        }
 
-        var tipwidth = '-' + (tipwidth - a_width)/2;
-        $('.tooltip').css({
-            'left' : tipwidth + 'px',
-            'bottom' : a_hegiht + 'px'
-        }).stop().animate({
-            opacity : 1
-        }, 200);
-       
+    //     var tipwidth = tooltip.outerWidth();
+    //     var a_width = $(this).width();
+    //     var a_hegiht = $(this).height() + 3 + 4;
 
-    });
+    //     //if the tooltip width is smaller than the a/link/parent width
+    //     if(tipwidth < a_width){
+    //         tipwidth = a_width;
+    //         $('.tooltip').outerWidth(tipwidth);
+    //     }
+
+    //     var tipwidth = '-' + (tipwidth - a_width)/2;
+    //     $('.tooltip').css({
+    //         'left' : tipwidth + 'px',
+    //         'bottom' : a_hegiht + 'px'
+    //     }).stop().animate({
+    //         opacity : 1
+    //     }, 200);
+
+
+    // });
 
     $("a").mouseout(function(){
-        var tooltip = $(".tooltip");       
+        var tooltip = $(".tooltip");
         tooltip.remove();
     });
 
 
 });
-
-
-
-
-
